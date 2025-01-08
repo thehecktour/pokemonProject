eee1edd536a7949c03e4080bd8d47780965b1b42
Atilio <atilio.jesus@grupoboticario.com.br>
2025-01-08 10:56:35 -0300

    chore: 🚧 add flask8 to insert lint inside the project

--- main.py
+++ main.py
@@ -6,18 +6,27 @@ import httpx
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def get_pokemon_form(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "abilities": []})
+    return templates.TemplateResponse("index.html", {"request": request, "abilities": [
+
+    ]})
+
 
 @app.post("/pokemon", response_class=HTMLResponse)
 async def fetch_pokemon_abilities(request: Request, pokemon_name: str = Form(...)):
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}")
+        url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
+        response = await client.get(url)
         if response.status_code == 200:
             response_data = response.json()
-            abilities = sorted([ability['ability']['name'] for ability in response_data['abilities']])
+            abilities = [
+                ability['ability']['name'] for ability in response_data['abilities']
+            ]
+            abilities = sorted(abilities)
         else:
             abilities = ["Pokémon not found"]
 
-    return templates.TemplateResponse("index.html", {"request": request, "abilities": abilities})
+    context = {"request": request, "abilities": abilities}
+    return templates.TemplateResponse("index.html", context)
